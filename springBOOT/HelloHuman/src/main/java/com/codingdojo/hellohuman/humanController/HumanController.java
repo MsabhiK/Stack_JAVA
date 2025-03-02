9263d6af9922dc786ca9c19b4bc105f3fead529c
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2025-03-03 00:20:09 +0100

    Add files via upload

---
+++ springBOOT/HelloHuman/src/main/java/com/codingdojo/hellohuman/humanController/HumanController.java
@@ -0,0 +1,43 @@
+package com.codingdojo.hellohuman.humanController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HumanController {
+	
+	@RequestMapping("/")
+	public String query(@RequestParam(value="name", required=false) String searchName, 
+			@RequestParam(value="lastname", required=false) String searchLast, 
+			@RequestParam(value="t", required=false) Integer times) {
+		
+		if(times != null && times > 0) {
+			String result = "";
+			for(int i = 0; i < times; i++) {
+				if(searchName != null && searchLast != null) {
+					result += "Hello " + searchName + " " + searchLast + " ";
+				}else if(searchName != null) {
+					result += "Hello " + searchName + " ";
+				}else if(searchLast != null) {
+					result += "Hello " + searchLast + " ";
+				}else {
+					result += "Hello Human ";
+				}
+			}
+			return result;
+		}
+		
+		if(searchName != null && searchLast !=null ) {
+			return"Hello " + searchName + " " + searchLast;
+		}
+		else if(searchName != null){
+			return"Hello: " + searchName;
+		}
+		else if(searchLast != null){
+			return"Hello: " + searchLast;
+		}
+		return "HELLO Human!!";
+	}
+
+}
