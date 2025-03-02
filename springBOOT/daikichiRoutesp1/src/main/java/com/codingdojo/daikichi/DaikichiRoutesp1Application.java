41873ee5335bde19d65116c68f336307bbab042e
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2025-03-02 18:25:48 +0100

    Add files via upload

---
+++ springBOOT/daikichiRoutesp1/src/main/java/com/codingdojo/daikichi/DaikichiRoutesp1Application.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRoutesp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesp1Application.class, args);
+	}
+
+}
