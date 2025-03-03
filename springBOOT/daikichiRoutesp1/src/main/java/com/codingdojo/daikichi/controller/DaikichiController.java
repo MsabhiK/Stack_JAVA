0b0017d1f4831fdd890cd600b0f747fa0ae847ab
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2025-03-03 01:19:10 +0100

    Update DaikichiController.java

--- springBOOT/daikichiRoutesp1/src/main/java/com/codingdojo/daikichi/controller/DaikichiController.java
+++ springBOOT/daikichiRoutesp1/src/main/java/com/codingdojo/daikichi/controller/DaikichiController.java
@@ -19,4 +19,17 @@ public class DaikichiController {
 	public String tomorrow() {
 		return"Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
+
+	@RequestMapping("/travel/{location}")
+	public String travel(@PathVariable("location") String location) {
+		return "Congratulations! You will soon travel to " + location + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") Integer number) {
+		if(number%2==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	}
 }
