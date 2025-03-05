f8023950a126f12338fd5c8812b6bac6ac3b7dc5
MsabhiK <151369797+MsabhiK@users.noreply.github.com>
2025-03-05 17:42:40 +0100

    Add files via upload

---
+++ springBOOT/display_Date/src/main/java/com/codingdojo/displayDate/controllers/MaindisplayDate.java
@@ -0,0 +1,35 @@
+package com.codingdojo.displayDate.controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MaindisplayDate {
+	@RequestMapping("/")
+	public String index() {
+		return"index.jsp";
+	}
+	
+	@RequestMapping("/date")
+	public String date(Model model) {
+		SimpleDateFormat format_date = new SimpleDateFormat("EEEE, MMM d, yyyy");
+		Date date = new Date();
+		model.addAttribute("date", format_date.format(date));
+		return "date.jsp";
+	}
+	
+	@RequestMapping("/time")
+	public String time(Model model) {
+		SimpleDateFormat format_time = new SimpleDateFormat("h:mm a");
+		
+		Date date = new Date();
+		
+		model.addAttribute("time", format_time.format(date));
+		
+		return "time.jsp";
+	}
+}
---
+++ springBOOT/display_Date/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<!--<link rel="stylesheet" href="/webjars/bootstrap/css/bootstrap.min.css" />-->
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<title>Dashboard</title>
+</head>
+<body>
+	<h1>Hello Sir for displaying DATE or TIME:</h1>
+	<div>
+		<h2><a href="/date">Date Template</a></h2>
+		<h2><a href="/time">Time Template</a></h2>
+	</div>
+
+</body>
+</html>
